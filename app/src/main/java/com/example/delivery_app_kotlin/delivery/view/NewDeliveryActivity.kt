0e99b4a5c70c266718714064096fb88fb7cc6866
Mekedelawit <mekedi.mkt@gmail.com>
2024-12-01 18:24:53 +0300

    request location permission

--- app/src/main/java/com/example/delivery_app_kotlin/delivery/view/NewDeliveryActivity.kt
+++ app/src/main/java/com/example/delivery_app_kotlin/delivery/view/NewDeliveryActivity.kt
@@ -1,13 +1,17 @@
 package com.example.delivery_app_kotlin.delivery.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.delivery_app_kotlin.auth.viewmodel.UserViewModel
 import com.example.delivery_app_kotlin.databinding.ActivityNewDeliveryBinding
 import com.example.delivery_app_kotlin.delivery.viewmodel.DeliveryViewModel
+import com.example.delivery_app_kotlin.main.view.MainActivity
 import com.example.delivery_app_kotlin.main.viewmodel.LocationViewModel
 
 class NewDeliveryActivity : AppCompatActivity() {
@@ -45,6 +49,9 @@ class NewDeliveryActivity : AppCompatActivity() {
             if (pickup.isNotEmpty() && dest.isNotEmpty() &&
                 deliveryViewModel.selectedDG.value != null
             ) {
+                if (!hasLocationPermission()) {
+                    requestLocationPermission()
+                }
 //                val locationViewModel = ViewModelProvider(this)[LocationViewModel::class.java]
 //                val userLocation = locationViewModel.userLocation.value
                 deliveryViewModel.addNewDelivery(applicationContext, pickup, dest)
@@ -59,6 +66,25 @@ class NewDeliveryActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+    private fun hasLocationPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            NewDeliveryActivity.LOCATION_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1001
     }
 
     private fun setUpRecyclerView() {
